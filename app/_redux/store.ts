37879127fd7dc36db682fc96e87c5aadbcf19c60
phaxz <phaxz.bautista10@gmail.com>
2024-01-18 22:25:14 +0800

    mend

--- app/_redux/store.ts
+++ app/_redux/store.ts
@@ -30,3 +30,4 @@ export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+export type AppStore = typeof store;
