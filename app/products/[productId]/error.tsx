07812525812aaf6f8cadd00bb505c64c205f17ab
phaxz <phaxz.bautista10@gmail.com>
2024-01-20 12:18:30 +0800

    added error page

---
+++ app/products/[productId]/error.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const ErrorPage = () => {
+  return (
+    <div className="grid h-[30vh] place-items-center p-6">
+      <h1 className="text-3xl font-bold text-center">Product not found</h1>
+    </div>
+  );
+};
+
+export default ErrorPage;
